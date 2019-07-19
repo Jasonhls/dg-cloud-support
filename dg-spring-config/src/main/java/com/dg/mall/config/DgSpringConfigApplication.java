084490b7281cdce98ebc0ce77640346c4d6b1922
hels <1597445096@qq.com>
2019-07-19 14:49:16 +0800

    添加配置中心组件

--- dg-spring-config/src/main/java/com/dg/mall/config/DgSpringConfigApplication.java
+++ dg-spring-config/src/main/java/com/dg/mall/config/DgSpringConfigApplication.java
@@ -1,17 +1,16 @@
-package com.dg.mall.admin;
+package com.dg.mall.config;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableAdminServer
+@EnableConfigServer
 @EnableEurekaClient
-public class DgSpringBootAdminApplication {
-
+public class DgSpringConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DgSpringBootAdminApplication.class, args);
+        SpringApplication.run(DgSpringConfigApplication.class,args);
     }
-
 }
